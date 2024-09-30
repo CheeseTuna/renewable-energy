407df722597d94acf8c2ca06562163365e5ad65d
CheeseTuna <cisefm123@gmail.com>
2024-09-30 21:54:58 +0100

    Initial commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import icons from "../../constants/icons";
+import "./NavBar.css";
+
+const NavBar = () => {
+  <nav>
+    <div className="app__navbar" style={{ color: "brown" }}>
+      <div className="app__navbar-logo">
+        <img src={wind.png} alt="wind turbine logo" />
+      </div>
+      <div>
+        <h1>Wind Turbine</h1>
+        <p>Monitor</p>
+      </div>
+    </div>
+  </nav>;
+};
+
+export default NavBar;
---
+++ src/constants/icons.js
@@ -0,0 +1,6 @@
+import wind from '../assets/wind.png';
+
+//eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    wind
+};
