47bef8217c162ed4f4f0ffb64e8f51b0a6bee0cd
CheeseTuna <cisefm123@gmail.com>
2024-10-02 14:44:42 +0100

    file structure changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
+import Overview from './general/Overview/Overview';
+
 
 const App = () => (
     <div>
         <NavBar/>
+        <Overview/>
+       
     </div>
 );
 
